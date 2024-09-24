90196033f1b7680b9a25125a6f353f69f0ee1991
Fidan4 <fidanas@code.edu.az>
2024-09-24 18:56:15 +0400

    Changes

--- Homework/Task1/Fidan.js
+++ Homework/Task1/Fidan.js
@@ -3,40 +3,40 @@
 // 2.Print each variable type using typeof.
 
 
-let Name= 'Fidan'
-console.log(typeof Name)
-let Age=20
-console.log(typeof Age)
-let Salary=undefined
-console.log(typeof Salary)
-let education=null
-console.log(typeof education)
-let isAdmin=true
-console.log(typeof isAdmin)
-let BigInt=4444n
-console.log(typeof BigInt)
-let Symbol1 = Symbol("private")
-console.log(typeof Symbol1)
+let name1 = 'Fidan'
+console.log(typeof name1);
+let age1 = 20
+console.log(typeof age1);
+let salary1 = undefined
+console.log(typeof salary1);
+let education = null
+console.log(typeof education);
+let isAdmin = true
+console.log(typeof isAdmin);
+let bigInt = 4444n
+console.log(typeof bigInt);
+let symbol= Symbol("private");
+console.log(typeof symbol)
 
 
 // Keywords:
 // 3.Use let, const, and var to declare variables and explain their differences with examples.
 
-let Name1 = "Fidan"
-console.log(Name1);
-Name1 = "Aysel"
-console.log(Name1);
+let name2 = "Fidan"
+console.log(name2);
+name2 = "Aysel"
+console.log(name2);
 // It is possible to change the value with let. However, we do not write let a second time.
 
 
-const Surname = "Sultanova"
-console.log(Surname);
+const surname = "Sultanova"
+console.log(surname);
 //With const, the value cannot be changed in any case.
 
-var Jobs = "CS"
-console.log(Jobs);
-var Jobs = "Network Admin"
-console.log(Jobs);
+var jobs = "CS"
+console.log(jobs);
+var jobs = "Network Admin"
+console.log(jobs);
 //It is possible to absorb new value when using var.
 
 
@@ -54,12 +54,13 @@ console.log (`My name is ${firstName} ${lastName} . I am ${age} . I live in ${co
 
 //  Operators:
 // 5.Use arithmetic operators (+, -, *, /, %).
-console.log(5 + 6);
+console.log(5 + 6);  
 console.log(11 - 5);
 console.log(5 * 6);
-console.log("5" + 6);
+console.log("5" + 6);  //56  //If there is a string, it is no longer interpreted as a number, but as a string
+console.log(30 / "6"); //5  //As you can see, this only applies to addition
 console.log(30 / 5);
-console.log(30 % 4);
+console.log(30 % 4);       //% indicates the remainder
 
 // 6.Use comparison operators (==, ===, >, <, etc.)
 let a = 26
@@ -77,16 +78,14 @@ let d = true
 let e = false
 let f = 58
 let g = 98
-console.log(d && e);
+console.log(d && e);             //&& means and
 console.log(f > 14 && g < 100);
 console.log(d || e);
-console.log(f > 14 || g < 100);
-console.log(!d);
+console.log(f > 14 || g < 100); // || or means
+console.log(!d);   //! expresses negation
 console.log(!e);
-
-
-
-
-
+// = gives value
+// == it only compares by type
+// === compares both by type and value
 
 
